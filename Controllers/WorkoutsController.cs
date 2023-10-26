7f38eec7d6983e8f598f8a5d1fe6e1b1c5558bdd
Eduard Robu <robu.edi1@gmail.com>
2023-10-26 12:58:53 +0100

    setting the workout controller body

--- Controllers/WorkoutsController.cs
+++ Controllers/WorkoutsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using web_api.Models;
 using web_api.Services;
 
 namespace web_api.Controllers;
@@ -11,5 +12,35 @@ public class WorkoutsController : ControllerBase
 
     public WorkoutsController(IWorkoutService workoutService)
         => _workoutService = workoutService;
+
+    [HttpGet]
+    public Task<ActionResult<IEnumerable<Workout>>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}")]
+    public Task<ActionResult<Workout>> Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public Task<IActionResult> Create(Workout workout)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}")]
+    public Task<IActionResult> Update(Guid id, Workout workout)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id}")]
+    public Task<IActionResult> Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
 
 }
